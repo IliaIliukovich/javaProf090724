040d21834e3c28700a2dccdd4b88d4fbb90031c4
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2024-10-15 10:39:18 +0200

    clone() method

---
+++ src/lesson20241015/Box.java
@@ -0,0 +1,71 @@
+package lesson20241015;
+
+public class Box implements Cloneable {
+
+     private String name;
+     private boolean canBeSold;
+     private int count;
+     private Cat cat;
+
+     public Box(String name, boolean canBeSold, int count, Cat cat) {
+          this.name = name;
+          this.canBeSold = canBeSold;
+          this.count = count;
+          this.cat = cat;
+     }
+
+     public void empty() {
+          cat = null;
+          name = null;
+          canBeSold = false;
+          count = 0;
+      }
+      public Box makeCopy() {
+           return new Box(name, canBeSold, count, cat);
+      }
+      public Box deepCopy() {
+          return new Box(name, canBeSold, count, new Cat(cat.getName(), cat.getAge(), cat.getColour()));
+      }
+
+    public static void main(String[] args) {
+        Cat cat = new Cat("My Cat", 2, "grey");
+        Box box = new Box("Box", true, 12, cat);
+
+//        Box box2 = box.makeCopy();
+        Box box2 = box.deepCopy();
+        System.out.println(box.cat == box2.cat);
+        box2.cat.setName("Box 2 cat");
+
+        System.out.println(box);
+
+        try {
+            Cat clonedCat = (Cat) cat.clone();
+            System.out.println(clonedCat);
+
+            Box clonedBox = (Box) box.clone();
+            System.out.println(clonedBox);
+
+            System.out.println(box.cat == clonedBox.cat);
+        } catch (CloneNotSupportedException e) {
+            System.out.println("Clone operation not supported");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Box{" +
+                "name='" + name + '\'' +
+                ", canBeSold=" + canBeSold +
+                ", count=" + count +
+                ", cat=" + cat +
+                '}';
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException { // deep copy
+        Box clone = (Box) super.clone(); // shallow copy
+        Cat clonedCat = (Cat) this.cat.clone();
+        clone.cat = clonedCat;
+        return clone;
+    }
+}
---
+++ src/lesson20241015/Cat.java
@@ -0,0 +1,52 @@
+package lesson20241015;
+
+public class Cat implements Cloneable {
+
+     private String name;
+     private int age;
+     private String colour;
+
+     public Cat(String name, int age, String colour) {
+          this.name = name;
+          this.age = age;
+          this.colour = colour;
+     }
+
+     public String getName() {
+          return name;
+     }
+
+     public int getAge() {
+          return age;
+     }
+
+     public String getColour() {
+          return colour;
+     }
+
+     public void setName(String name) {
+          this.name = name;
+     }
+
+     public void setAge(int age) {
+          this.age = age;
+     }
+
+     public void setColour(String colour) {
+          this.colour = colour;
+     }
+
+     @Override
+     public String toString() {
+          return "Cat{" +
+                  "name='" + name + '\'' +
+                  ", age=" + age +
+                  ", colour='" + colour + '\'' +
+                  '}';
+     }
+
+     @Override
+     public Object clone() throws CloneNotSupportedException {
+          return super.clone();
+     }
+}
