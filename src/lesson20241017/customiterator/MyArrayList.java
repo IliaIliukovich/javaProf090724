652b0e5571ca3a2ec4686967b759bd9f7ee82aff
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2024-10-17 12:54:04 +0200

    Custom iterator

--- src/lesson20241017/customiterator/MyArrayList.java
+++ src/lesson20241017/customiterator/MyArrayList.java
@@ -7,12 +7,12 @@ public class MyArrayList implements Iterable<String> {
 
     private String[] data;
 
-    public void add(String element) {
-        // logic for adding element
-    }
-
     public MyArrayList(String[] data) {
         this.data = data;
+    }
+
+    public void add(String element) {
+        // logic for adding element
     }
 
     @Override
@@ -39,14 +39,18 @@ public class MyArrayList implements Iterable<String> {
     }
 
     private class MyReverseIterator implements Iterator<String> {
+        private int index = data.length - 1;
         @Override
         public boolean hasNext() {
-            return false;
+            return index >= 0;
         }
 
         @Override
         public String next() {
-            return null;
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return data[index--];
         }
     }
 
--- src/lesson20241017/customiterator/UseMyArrayList.java
+++ src/lesson20241017/customiterator/UseMyArrayList.java
@@ -7,7 +7,7 @@ public class UseMyArrayList {
     public static void main(String[] args) {
         MyArrayList myArrayList = new MyArrayList(new String[] {"A", "B", "C", "D", "E"});
 
-        for (String s : myArrayList) { // E --> D --> C --> B --> A
+        for (String s : myArrayList) { // revereIterator E --> D --> C --> B --> A
             String element = s;
             System.out.println(element);
         }
@@ -15,6 +15,21 @@ public class UseMyArrayList {
 //        Iterator<String> iterator = myArrayList.iterator();
 //        System.out.println(iterator.next());
 
+        myArrayList = new MyArrayList(new String[0]);
+        for (String s : myArrayList) {
+            String element = s;
+            System.out.println(element);
+        }
+
+        myArrayList = new MyArrayList(new String[] {"A"});
+        for (String s : myArrayList) {
+            String element = s;
+            System.out.println(element);
+        }
+
+        Iterator<String> iterator = myArrayList.iterator();
+        iterator.next();
+//        iterator.next(); // impossible
 
     }
 
