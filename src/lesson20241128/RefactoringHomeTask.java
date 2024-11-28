a400f11abb4b6207c57a7f8c9d06427b9d2db4e6
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2024-11-28 10:00:42 +0100

    hometask

---
+++ src/lesson20241128/RefactoringHomeTask.java
@@ -0,0 +1,41 @@
+package lesson20241128;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class RefactoringHomeTask {
+
+    public static void main(String[] args) {
+
+        List<Double> doubleList = List.of(1.0, 2.0, 3.0, 4.0);
+
+//        List<Double> doubles = new ArrayList<>();
+//        for (Double d : doubleList) {
+//            if (d % 2 == 1) {
+//                doubles.add(d);
+//            }
+//        }
+//        System.out.println(doubles);
+
+        List<Double> doubles = doubleList.stream().filter(d -> d % 2 == 1).collect(Collectors.toCollection(ArrayList::new));
+        System.out.println(doubles);
+
+
+        List<String> list = List.of("One", "Two", "Three", "Four", "Five");
+        String word = null;
+        for (String t : list){
+            if (t.length() == 3) {
+                word = t;
+                break;
+            }
+        }
+        System.out.println(word);
+
+
+
+
+    }
+
+
+}
