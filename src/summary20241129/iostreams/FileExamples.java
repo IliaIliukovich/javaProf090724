5d1e6fa0dbff67a0d55492edb6b63d3927ef1954
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2024-11-29 11:42:22 +0100

    File class

---
+++ src/summary20241129/iostreams/FileExamples.java
@@ -0,0 +1,23 @@
+package summary20241129.iostreams;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class FileExamples {
+
+    public static void main(String[] args) {
+        File fileOrFolder = new File("resources/file.txt");
+        System.out.println(fileOrFolder.exists());
+        System.out.println(fileOrFolder.isFile());
+
+        fileOrFolder = new File("resources/newFolder");
+        fileOrFolder.mkdir();
+        System.out.println(fileOrFolder.exists());
+        String[] listed = fileOrFolder.list();
+        System.out.println(Arrays.toString(listed));
+
+    }
+
+
+
+}
