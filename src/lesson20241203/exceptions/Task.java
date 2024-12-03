69940003c060d0732fade4d38ed8dd0b7886db11
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2024-12-03 11:31:20 +0100

    Exceptions

--- src/lesson20241203/exceptions/Task.java
+++ src/lesson20241203/exceptions/Task.java
@@ -7,14 +7,39 @@ public class Task {
     // в качестве параметра и выдает проверяемое исключение, если число нечетное.
 //        throw new RuntimeException("Unchecked");
 //        throw new Exception("Checked");
-
+        try {
+            method(2);
+            method(1);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
 
     // 2. Написать метод проверки введенного пароля. Если пароль неверен, выбрасывается проверяемое исключение
     // WrongPasswordException с сообщением "Password is wrong".
+        try {
+            validatePassword("12345");
+            validatePassword("1234");
+        } catch (WrongPasswordException e) {
+            System.out.println("Password is wrong. Try again");
+        }
 
+        System.out.println("Some logic");
 
+    }
 
+    public static void method(int number) throws Exception {
+        if (number % 2 == 1) {
+            throw new Exception("Exception");
+        }
+        System.out.println(number);
+    }
 
+    public static void validatePassword(String password) throws WrongPasswordException{
+        String correctPassword = "12345";
+        if (!correctPassword.equals(password)) {
+            throw new WrongPasswordException();
+        }
+        System.out.println("Password is correct");
     }
 
 
---
+++ src/lesson20241203/exceptions/WrongPasswordException.java
@@ -0,0 +1,4 @@
+package lesson20241203.exceptions;
+
+public class WrongPasswordException extends Exception {
+}
