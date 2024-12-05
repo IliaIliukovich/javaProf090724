69646651aa019f7d8bce5ff4a6f4ee1b83224a93
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2024-12-05 18:47:13 +0100

    cleanup

--- src/lesson20241205/regex/RegexExamples.java
+++ src/lesson20241205/regex/RegexExamples.java
@@ -13,13 +13,22 @@ public class RegexExamples {
         Matcher matcher = pattern.matcher(text);
         System.out.println(matcher.matches());
 
+        //  .	Любой отдельный символ, кроме новой строки
+        // \w	Слово из символов
+        // \W	Несловесные символы
+        // \s	Соответствует пробелу
+        // \S	без пробелов
+        // \d	Соответствует цифрам. Эквивалентно [0-9]
+        // \D	Совпадает с “не цифрами”
+
         System.out.println(Pattern.matches("world\\.", "world.")); // . in the end
         System.out.println(Pattern.matches("world.", "world!")); // any symbol in the end
         System.out.println(Pattern.matches("[ws]orld", "world")); // w or s
         System.out.println(Pattern.matches("[ws]orld", "sorld")); // w or s
         System.out.println(Pattern.matches("world[!?.]", "world?")); // ! or ? or .
         System.out.println(Pattern.matches("world[^!?.]", "world1")); // any except ! ? .
-        System.out.println(Pattern.matches("[0]{3}[0-9]{5}", "00012345")); //
+
+        System.out.println(Pattern.matches("[0]{3}[0-9]{5}", "00012345"));
         System.out.println(Pattern.matches("[a-zA-Züäö]{3}[0-9]{5}", "aöC12345"));
         System.out.println(Pattern.matches("[\\w]{3}", "a_1")); // a-zA-Z_0-9
         System.out.println(Pattern.matches("[\\W]{3}", ",?."));
