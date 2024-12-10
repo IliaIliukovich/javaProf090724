ec5c3e3f1c7aed8b37db19af45f009e5312799c1
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2024-12-10 13:00:05 +0100

    maven support

---
+++ src/test/java/summary20241206/Task5Test.java
@@ -0,0 +1,12 @@
+package summary20241206;
+
+import org.junit.jupiter.api.Test;
+
+public class Task5Test {
+
+    @Test
+    public void testVerify() {
+        boolean result = Task5.verify("12345678Aa@");
+    }
+
+}
